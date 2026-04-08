258644a7d08e8f2bc35bf530b66c56bbc97780b7
SDaniel0210 <samudanielbotond@gmail.com>
2026-04-08 16:42:16 +0200

    Import button functioning and partial data saving in db

--- app/src/main/java/com/example/photometa/MainActivity.java
+++ app/src/main/java/com/example/photometa/MainActivity.java
@@ -19,15 +19,61 @@ import android.content.Intent;
 import android.provider.MediaStore;
 import android.view.View;
 
+import android.net.Uri;
+import android.widget.Toast;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import java.util.List;
+
+import androidx.room.Room;
+import com.example.photometa.data.local.AppDatabase;
+import com.example.photometa.data.local.entity.Photo;
+
 public class MainActivity extends AppCompatActivity {
 
     private NavController navController;
+    private ActivityResultLauncher<String[]> imagePickerLauncher;
+    private AppDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        db = Room.databaseBuilder(
+                getApplicationContext(),
+                AppDatabase.class,
+                "photo_db"
+        ).build();
+
+        imagePickerLauncher = registerForActivityResult(
+                new ActivityResultContracts.OpenMultipleDocuments(),
+                uris -> {
+                    if (uris != null && !uris.isEmpty()) {
+                        Toast.makeText(this, uris.size() + " image selected", Toast.LENGTH_SHORT).show();
+
+                        new Thread(() -> {
+                            for (Uri uri : uris) {
+                                Photo photo = new Photo();
+                                photo.setImageUri(uri.toString());
+                                photo.setAiStatus("UNKNOWN");
+
+                                db.photoDao().insert(photo);
+                            }
+
+                            int count = db.photoDao().getAll().size();
+
+                            runOnUiThread(() ->
+                                    Toast.makeText(this, "DB count: " + count, Toast.LENGTH_LONG).show()
+                            );
+                        }).start();
+
+                    } else {
+                        Toast.makeText(this, "No image selected", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
 
 
         //rng placeholder image on startup
@@ -45,6 +91,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivity(cameraIntent);
+            }
+        });
+
+        Button import_btn = findViewById(R.id.import_btn);
+
+        import_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imagePickerLauncher.launch(new String[]{"image/*"});
             }
         });
 
